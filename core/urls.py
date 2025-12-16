0be7b970c9e4d79d76972a3d13cf27165cd36c71
dg_nvrr <diego.arenas.navarro@gmail.com>
2025-12-16 13:55:45 -0300

    Permite sevir imagenes en produccion

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.urls import re_path
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,3 +25,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# ... (tu código anterior) ...
+
+# --- AGREGAR ESTO AL FINAL ---
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
